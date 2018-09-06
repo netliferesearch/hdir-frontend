1c7384e424d3ebf04c3106b2d2642a47090e684e
Ole Magnus Støvern <>
2018-09-06 12:58:56 +0200

    adds grid example and removes unused code

--- src/App.js
+++ src/App.js
@@ -1,18 +1,31 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <h1 className="h1">Rundskriv og veiledere til lov og forskrift</h1>
+        <p>Dette er noe tekst som er under.</p>
+        <div className="col-xs">
+          <div className="row center-xs">
+            <div
+              className="col-xs-12
+              col-sm-8
+              col-md-6
+              col-lg-4"
+            >
+              <div className="box">Responsive</div>
+            </div>
+          </div>
+        </div>
+        <div className="col-xs">
+          <div className="row">
+            <div className="col-xs-12">
+              <div className="box2">Wide</div>
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
