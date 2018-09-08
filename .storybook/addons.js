42abcca9130ace5ae8cdb499e166474d600543d3
Ole Magnus Støvern <>
2018-09-08 12:10:48 +0200

    Adds viewport addon

--- .storybook/addons.js
+++ .storybook/addons.js
@@ -1,3 +1,4 @@
 import '@storybook/addon-actions/register';
 import '@storybook/addon-links/register';
 import 'react-storybook-addon-static-markup/register';
+import '@storybook/addon-viewport/register';
