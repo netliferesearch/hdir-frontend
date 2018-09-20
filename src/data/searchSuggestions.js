c7fd3539023e99f64fa1c1770fb353fe76bc8799
Ole Magnus Støvern <>
2018-09-20 15:37:53 +0200

    Adds helfo suggestions

--- src/data/searchSuggestions.js
+++ src/data/searchSuggestions.js
@@ -1,4 +1,5 @@
 export default [
+  // HDIR:
   {
     title: 'Svangerskapsdiabetes',
     intro: 'Retningslinjer, anbefalinger og informasjon om svangerspasdiabetes',
@@ -34,6 +35,68 @@ export default [
       'Svangerskapsdiabetes er diabetes som oppdages eller oppstår i graviditeten. Det kan gi økt risiko for svangerskapsforgiftning og føre til komplikasjoner under fødselen, blant annet fordi barnet kan få høy fødselsvekt.',
     type: 'helsenorge.no',
     topics: [''],
+    publishDate: '',
+    editDate: '',
+    codes: []
+  },
+  {
+    title: 'Frikort for helsetjenester',
+    intro:
+      'Privatpersoner får frikort for helsetjenester når de har betalt over et visst beløp i egenandeler. Behandlere og tjenesteytere må rapportere alle godkjente egenandeler minst hver 14. dag.',
+    type: 'temaside',
+    topics: ['frikort'],
+    targetGroups: ['helseaktør'],
+    publishDate: '2018-07-17T03:24:00',
+    editDate: '2018-07-17T03:24:00',
+    codes: []
+  },
+  {
+    title: 'Rapportering av egenandeler frikort tak 2',
+    intro:
+      'I egenandelstak 2 inngår egenandeler for undersøkelse og behandling hos fysioterapeut, både hos fysioterapeuter med driftstilskudd og fastlønnede fysioterapeuter i kommunene.',
+    type: 'temaside',
+    topics: ['frikort', 'rapportering'],
+    targetGroups: ['helseaktør'],
+    publishDate: '2018-07-17T03:24:00',
+    editDate: '2018-07-17T03:24:00',
+    codes: []
+  },
+
+  // HELFO:
+  {
+    title: 'Egenandelstak 1 for 2018: 2258 kroner',
+    intro: 'Taket for frikort egenandelstak 1 er 2 258 kroner for 2018. ',
+    type: 'regelverk',
+    topics: ['frikort'],
+    targetGroups: [
+      'apotek',
+      'lege',
+      'psykolog',
+      'sykehus',
+      'pasientreiser',
+      'laboratorier'
+    ],
+    publishDate: '',
+    editDate: '',
+    codes: []
+  },
+  {
+    title: 'Egenandelstak 2 for 2018: 2025 kroner',
+    intro: 'Taket for frikort egenandelstak 2 er 2 025 kroner for 2018. ',
+    type: 'regelverk',
+    topics: ['frikort'],
+    targetGroups: ['fysioterapeut'],
+    publishDate: '',
+    editDate: '',
+    codes: []
+  },
+  {
+    title: 'Frikort for helsetjenester',
+    intro:
+      'Frikort for helsetjenester får du når du har betalt over et visst beløp i egenandeler. Når du viser frikort, slipper du å betale egenandeler for resten av kalenderåret.',
+    type: 'helsenorge.no',
+    topics: ['frikort'],
+    targetGroups: ['privatperson'],
     publishDate: '',
     editDate: '',
     codes: []
