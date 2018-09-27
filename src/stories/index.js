f725c47312a8eb8f333a132f426e6e9baa0d69d7
Ole Magnus Støvern <>
2018-09-27 14:49:58 +0200

    CSS style changer

--- src/stories/index.js
+++ src/stories/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
-import { linkTo } from '@storybook/addon-links';
-import { withKnobs, text, boolean, object } from '@storybook/addon-knobs';
+// import { linkTo } from '@storybook/addon-links';
+import { withKnobs, text, boolean } from '@storybook/addon-knobs';
 import { checkA11y } from '@storybook/addon-a11y';
 import { withNotes } from '@storybook/addon-notes';
 
@@ -21,7 +21,22 @@ import InputSearch from '../components/InputSearch';
 // Test data
 import { featureListHeading, featureListData } from '../testData.js';
 
-storiesOf('Welcome', module).add('to Storybook', () => (
+// TODO: The Storybook-devs are working on an official css toggle addon
+// Also remember to remove the copy function in the NPM storybook building script
+const changeStyle = name => {
+  const style = document.getElementById('main-style');
+  console.log('click my dude');
+  if (name === 'hdir') style.setAttribute('href', 'static/hdir.css');
+  else if (name === 'helfo') style.setAttribute('href', 'static/helfo.css');
+  else {
+    console.log('Undefined style name');
+    return;
+  }
+  document.querySelector('head style') &&
+    document.querySelector('head style').remove();
+};
+
+storiesOf('Welcome', module).add('Styles', () => (
   <div>
     <h1 className="h2">
       Digitial design guide for DIP - Digital Innholdsplattform
@@ -29,14 +44,33 @@ storiesOf('Welcome', module).add('to Storybook', () => (
     <p>Velg stilsett:</p>
     <ul>
       <li>
-        <a href="#">Helsedirektoratet</a>
+        <button
+          onClick={() => {
+            changeStyle('hdir');
+          }}
+        >
+          Helsedirektoratet
+        </button>
       </li>
       <li>
-        <a href="#">Helfo</a>
+        <button
+          onClick={() => {
+            changeStyle('helfo');
+          }}
+        >
+          Helfo
+        </button>
       </li>
     </ul>
   </div>
 ));
+
+storiesOf('Pages', module)
+  .addDecorator(withKnobs)
+  .addDecorator(checkA11y)
+  .addWithStaticMarkup('Front page', () => <FrontPage />)
+  .addWithStaticMarkup('Search result page', () => <SearchResultPage />)
+  .addWithStaticMarkup('Article page', () => <ArticlePage />);
 
 storiesOf('FeatureList', module)
   .addDecorator(withKnobs)
@@ -65,13 +99,6 @@ storiesOf('InputSearch', module)
   .addWithStaticMarkup('Basic', () => (
     <InputSearch dark={boolean('Dark mode', false)} />
   ));
-
-storiesOf('Pages', module)
-  .addDecorator(withKnobs)
-  .addDecorator(checkA11y)
-  .addWithStaticMarkup('Front page', () => <FrontPage />)
-  .addWithStaticMarkup('Search result page', () => <SearchResultPage />)
-  .addWithStaticMarkup('Article page', () => <ArticlePage />);
 
 storiesOf('NavList', module)
   .addDecorator(withKnobs)
