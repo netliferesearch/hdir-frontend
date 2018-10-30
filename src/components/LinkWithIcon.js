9baff9b5c62042d12a76ad6b882a82070c7257bf
olemagnus <olestovern@gmail.com>
2018-10-30 20:30:20 +0100

    Makes icon not required

--- src/components/LinkWithIcon.js
+++ src/components/LinkWithIcon.js
@@ -3,11 +3,20 @@ import PropTypes from 'prop-types';
 import classNames from 'classnames';
 
 const linkClasses = (arrow, small) =>
-  classNames({ 'b-link-with-icon__link': true, 'b-link-with-icon__arrow': arrow, 'b-link-with-icon__small': small });
+  classNames({
+    'b-link-with-icon__link': true,
+    'b-link-with-icon__arrow': arrow,
+    'b-link-with-icon__small': small
+  });
 
 const LinkWithIcon = props => (
   <div className="b-link-with-icon">
-    <div className="b-link-with-icon__icon" style={{ backgroundImage: `url(${props.icon})` }}></div>
+    {props.icon && (
+      <div
+        className="b-link-with-icon__icon"
+        style={{ backgroundImage: `url(${props.icon})` }}
+      />
+    )}
     <a href={props.href} className={linkClasses(props.arrow, props.small)}>
       {props.children}
     </a>
@@ -16,9 +25,9 @@ const LinkWithIcon = props => (
 
 LinkWithIcon.propTypes = {
   href: PropTypes.string.isRequired,
-  icon: PropTypes.string.isRequired,
+  icon: PropTypes.string,
   arrow: PropTypes.bool,
   small: PropTypes.bool
-}
+};
 
 export default LinkWithIcon;
