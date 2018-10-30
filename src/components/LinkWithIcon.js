2c0b749b0bc070a64c74b1d6df00830efaf55ab6
olemagnus <olestovern@gmail.com>
2018-10-30 16:15:30 +0100

    adds arrow prop

--- src/components/LinkWithIcon.js
+++ src/components/LinkWithIcon.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import classNames from 'classnames';
+
+const linkClasses = arrow =>
+  classNames({ 'b-link-with-icon__link': true, 'b-link-with-icon__arrow': arrow });
 
 const LinkWithIcon = props => (
   <div className="b-link-with-icon">
     <div className="b-link-with-icon__icon" style={{ backgroundImage: `url(${props.icon})` }}></div>
-    <a href={props.href}>
+    <a href={props.href} className={linkClasses(props.arrow)}>
       {props.children}
     </a>
   </div>
@@ -12,7 +16,8 @@ const LinkWithIcon = props => (
 
 LinkWithIcon.propTypes = {
   href: PropTypes.string.isRequired,
-  icon: PropTypes.string.isRequired
+  icon: PropTypes.string.isRequired,
+  arrow: PropTypes.bool
 }
 
 export default LinkWithIcon;
