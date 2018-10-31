93ed976a1b33c9808705eec0f6ada37ef329bf60
Ådne Forslund <adne.forslund@gmail.com>
2018-10-31 14:24:50 +0100

    add some nav elements

--- src/pages/LisFrontPage.js
+++ src/pages/LisFrontPage.js
@@ -4,7 +4,6 @@ import NavList from '../components/NavList';
 import PageMeta from '../components/PageMeta';
 import ChapterHeading from '../components/ChapterHeading';
 import Quote from '../components/Quote';
-import FilterList from '../components/FilterList';
 import Collapsible from '../components/Collapsible';
 import ArticleIntro from '../components/ArticleIntro';
 
@@ -17,8 +16,106 @@ const LisFrontPage = () => (
           lead="Arbeidslivet bidrar til mange helsefremmende faktorer for de fleste. Arbeid er med på å sikre personlig økonomi, gi struktur i hverdagen, skape tilhørighet og gi økt selvfølelse."
         />
       </div>
-      <hr className="b-hr b-hr--less-thick b-hr--black" />
-      <NavList list={[{ title: 'LIS del 1', url: '#' }]} />
+      <div className="l-mt-3">
+        <hr className="b-hr b-hr--less-thick b-hr--black" />
+        <NavList list={[{ title: 'LIS del 1', url: '#' }]} />
+      </div>
+
+      <article className="l-mt-4">
+        <div className="l-mb-3">
+          <h2 className="l-mb-3">Velg spesialitet</h2>
+          <NavList
+            heading="A"
+            columns
+            list={[
+              {
+                title: 'Akutt og mottaksmedisin',
+                url: '#'
+              },
+              {
+                title: 'Allmenmedisin',
+                url: '#'
+              },
+              {
+                title: 'Anestesiologi',
+                url: '#'
+              },
+              {
+                title: 'Arbeidsmedisin',
+                url: '#'
+              }
+            ]}
+          />
+        </div>
+        <div className="l-mb-3">
+          <NavList
+            heading="B"
+            columns
+            list={[
+              {
+                title: 'Barne- og ungdomspsykiatri',
+                url: '#'
+              },
+              {
+                title: 'Barnekirurgi',
+                url: '#'
+              },
+              {
+                title: 'Barnesykdommer',
+                url: '#'
+              }
+            ]}
+          />
+        </div>
+        <div className="l-mb-3">
+          <NavList
+            heading="G"
+            columns
+            list={[
+              {
+                title: 'Gastrosentrisk kirurgi',
+                url: '#'
+              },
+              {
+                title: 'Geriatri',
+                url: '#'
+              }
+            ]}
+          />
+        </div>
+        <div className="l-mb-3">
+          <NavList
+            heading="H"
+            columns
+            list={[
+              {
+                title: 'Hjertesykdommer',
+                url: '#'
+              },
+              {
+                title: 'Hud- og veneriske sykdommer',
+                url: '#'
+              }
+            ]}
+          />
+        </div>
+        <div className="l-mb-3">
+          <NavList
+            heading="I"
+            columns
+            list={[
+              {
+                title: 'Immunologi og transfusjonsmedisin',
+                url: '#'
+              },
+              {
+                title: 'Indremedisin',
+                url: '#'
+              }
+            ]}
+          />
+        </div>
+      </article>
       <article className="col-md-8 col-xs-12 col-md-offset-1 t-body-text">
         <div className="l-mt-4">
           <PageMeta publishDate="11.08.2008" editDate="11.08.2018" />
