6ce51eb921d99259863d6a767c41aeec72658960
Ådne Forslund <adne.forslund@gmail.com>
2018-11-05 12:09:35 +0100

    add option for white color

--- src/components/Box.js
+++ src/components/Box.js
@@ -3,14 +3,18 @@ import PropTypes from 'prop-types';
 import classNames from 'classnames';
 
 const boxClasses = color =>
-  classNames({ 'b-box': true, 'b-box--green': color === 'green' });
+  classNames({
+    'b-box': true,
+    'b-box--green': color === 'green',
+    'b-box--white': color === 'white'
+  });
 
 const Box = props => (
   <section className={boxClasses(props.color)}>{props.children}</section>
 );
 
 Box.propTypes = {
-  color: PropTypes.oneOf('green')
+  color: PropTypes.oneOf('green', 'white')
 };
 
 export default Box;
