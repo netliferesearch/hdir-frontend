d526d5028fa3111b356d5ac62fd244029428d849
olemagnus <olestovern@gmail.com>
2018-11-15 10:18:24 +0100

    Legger pages på forsiden

--- src/components/MainHeader.js
+++ src/components/MainHeader.js
@@ -13,71 +13,8 @@ const ContentLabel = props => {
       </Link>
 
       <nav className="b-main-header__nav" style={{ fontSize: '12px' }}>
-        <Link to="/kapittel" className="b-main-header__link">
-          Kapittel
-        </Link>
-        <Link to="/produkt" className="b-main-header__link">
-          Produkt
-        </Link>
-        {/*<Link to="/søkeresultat" className="b-main-header__link">
-          SERP
-        </Link>*/}
-        <Link to="/søkeresultat2" className="b-main-header__link">
-          Enkel SERP
-        </Link>
-        <Link to="/artikkel" className="b-main-header__link">
-          Artikkel
-        </Link>
-        <Link to="/normerende-innhold" className="b-main-header__link">
-          Normerende
-        </Link>
-        <Link to="/høringer-forside" className="b-main-header__link">
-          Høringer forside
-        </Link>
-        <Link to="/høringer" className="b-main-header__link">
-          Høringer
-        </Link>
-        <Link to="/anbefaling" className="b-main-header__link">
-          Anbefaling
-        </Link>
-        <Link to="/rundskriv" className="b-main-header__link">
-          Rundskriv
-        </Link>
-        <Link to="/rundskriv-forside" className="b-main-header__link">
-          Rundskriv forside
-        </Link>
-        <Link to="/rapport" className="b-main-header__link">
-          Rapport
-        </Link>
-        <Link to="/lis-spesialitet" className="b-main-header__link">
-          LIS-spesialitet
-        </Link>
-        <Link to="/lis-læringsmål" className="b-main-header__link">
-          LIS-læringsmål
-        </Link>
-        <Link to="/lis-forside" className="b-main-header__link">
-          LIS-forside
-        </Link>
-        <Link to="/lis-læringsaktiviteter" className="b-main-header__link">
-          LIS-læringsaktiviteter
-        </Link>
-        <Link to="/rapport-kapittel" className="b-main-header__link">
-          Rapport kapittel
-        </Link>
-        <Link to="/yrke-velger" className="b-main-header__link">
-          Yrke velger
-        </Link>
-        <Link to="/autorisasjon-forside" className="b-main-header__link">
-          Autorisasjon forside
-        </Link>
-        <Link to="/autorisasjon-steg" className="b-main-header__link">
-          Autorisasjon steg
-        </Link>
-        <Link to="/autorisasjon-steg-2" className="b-main-header__link">
-          Autorisasjon steg 2
-        </Link>
-        <Link to="/rapportside-uten-bilde" className="b-main-header__link">
-          Rapportside uten bilde
+        <Link to="/" className="b-main-header__link">
+          Frontpage
         </Link>
       </nav>
     </div>
--- src/pages/FrontPage.js
+++ src/pages/FrontPage.js
@@ -1,22 +1,96 @@
 import React from 'react';
 import InputSearch from '../components/InputSearch';
+import { Link } from 'react-router-dom';
 
 const FrontPage = () => (
-  <div className="frontpage-wrapper">
-    <div className="l-container">
-      <p className="some-label">
-        Søk etter reningslinjer, anbefalinger, lover, statistikk, tilskudd...
-      </p>
-      <InputSearch id="x" dark />
-    </div>
-    <br />
-    <br />
-    <br />
-    <h3 className="some-label">
-      Press 'h' to toggle block/component hints.
+  <div>
+    <div className="frontpage-wrapper">
+      <div className="l-container">
+        <p className="some-label">
+          Søk etter reningslinjer, anbefalinger, lover, statistikk, tilskudd...
+        </p>
+        <InputSearch id="x" dark />
+      </div>
       <br />
-      Press 'j' to add Helfo styling. Refresh page to undo.
-    </h3>
+      <br />
+      <br />
+      <h3 className="some-label">
+        Press 'h' to toggle block/component hints.
+        <br />
+        Press 'j' to add Helfo styling. Refresh page to undo.
+      </h3>
+    </div>
+    <div className="l-container l-mt-3">
+      <h2>Pages</h2>
+      <p>
+        <Link to="/artikkel">Artikkel</Link>
+      </p>
+      <p>
+        <Link to="/anbefaling">Anbefaling</Link>
+      </p>
+      <p>
+        <Link to="/autorisasjon-forside">Autorisasjon forside</Link>
+      </p>
+      <p>
+        <Link to="/autorisasjon-steg">Autorisasjon steg</Link>
+      </p>
+      <p>
+        <Link to="/autorisasjon-steg-2">Autorisasjon steg 2</Link>
+      </p>
+      <p>
+        <Link to="/søkeresultat2">Enkel SERP</Link>
+      </p>
+      <p>
+        <Link to="/høringer-forside">Høringer forside</Link>
+      </p>
+      <p>
+        <Link to="/høringer">Høringer</Link>
+      </p>
+
+      <p>
+        <Link to="/normerende-innhold">Normerende</Link>
+      </p>
+      <p>
+        <Link to="/kapittel">Kapittel</Link>
+      </p>
+      <p>
+        <Link to="/lis-spesialitet">LIS-spesialitet</Link>
+      </p>
+      <p>
+        <Link to="/lis-læringsmål">LIS-læringsmål</Link>
+      </p>
+      <p>
+        <Link to="/lis-forside">LIS-forside</Link>
+      </p>
+      <p>
+        <Link to="/lis-læringsaktiviteter">LIS-læringsaktiviteter</Link>
+      </p>
+      <p>
+        <Link to="/produkt">Produkt</Link>
+      </p>
+      <p>
+        <Link to="/rundskriv">Rundskriv</Link>
+      </p>
+      <p>
+        <Link to="/rundskriv-forside">Rundskriv forside</Link>
+      </p>
+      <p>
+        <Link to="/rapport">Rapport</Link>
+      </p>
+      <p>
+        <Link to="/rapportside-uten-bilde">Rapportside uten bilde</Link>
+      </p>
+      <p>
+        <Link to="/rapport-kapittel">Rapport kapittel</Link>
+      </p>
+
+      <p>
+        <Link to="/yrke-velger">Yrke velger</Link>
+      </p>
+      {/*<Link to="/søkeresultat">
+          SERP
+        </Link>*/}
+    </div>
   </div>
 );
 
