f63ed3c840a5caf4966d52285813009d22ee6c13
olemagnus <olestovern@gmail.com>
2018-11-20 13:17:44 +0100

    Adds listheading

--- src/components/Select.js
+++ src/components/Select.js
@@ -3,22 +3,37 @@ import PropTypes from 'prop-types';
 import shortid from 'shortid';
 import classNames from 'classnames';
 
-const selectClasses = stacked =>
-  classNames({ 'b-select': true, 'b-select--stacked': stacked });
+const selectClasses = (stacked, simple) =>
+  classNames({
+    'b-select': true,
+    'b-select--stacked': stacked,
+    'b-select--simple': simple
+  });
+
+const selectElementClasses = simple =>
+  classNames({
+    'b-select__select': true,
+    'b-select__select--simple': simple
+  });
 
 const Select = props => (
-  <div className={selectClasses(props.stacked)}>
+  <div className={selectClasses(props.stacked, props.simple)}>
     <label htmlFor="contentCustomizer" className="b-select__label">
       {props.label}
     </label>
-    <select name="contentCustomizer" className="b-select__select">
+    <select
+      name="contentCustomizer"
+      className={selectElementClasses(props.simple)}
+    >
       {/* This value is suppose to be empty.
           It should also have the attribute selected. defaultValue should be value.
           JSX requires we write it this way.
       */}
-      <option defaultValue="" disabled className="b-select__option">
-        {props.placeholder}
-      </option>
+      {props.placeholder && (
+        <option defaultValue="" disabled className="b-select__option">
+          {props.placeholder}
+        </option>
+      )}
       {props.options.map(option => (
         <option key={shortid.generate()} className="b-select__option">
           {option}
@@ -32,11 +47,8 @@ Select.propTypes = {
   label: PropTypes.string,
   placeholder: PropTypes.string,
   options: PropTypes.arrayOf(PropTypes.string),
-  stacked: PropTypes.bool
-};
-
-Select.defaultProps = {
-  placeholder: 'Velg'
+  stacked: PropTypes.bool,
+  simple: PropTypes.bool
 };
 
 export default Select;
