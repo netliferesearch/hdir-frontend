08ddbcf153d2925552f4ef94169cde8e1d4be90f
Ådne Forslund <adne.forslund@gmail.com>
2018-11-22 17:06:25 +0100

    add more tema

--- src/pages/HdirTema.js
+++ src/pages/HdirTema.js
@@ -10,6 +10,7 @@ import Button from '../components/Button';
 import Heading from '../components/Heading';
 import ArticleIntro from '../components/ArticleIntro';
 import Card from '../components/Card';
+import Statistics from '../components/Statistics';
 const HdirTema = () => (
   <div>
     <div className="l-container">
@@ -76,20 +77,43 @@ const HdirTema = () => (
             />
           </div>
         </div>
-        <div className="col-xs-12 l-mt-5 l-mb-4">
-          <Heading h="h2">Tilskudd</Heading>
-          <hr className="b-hr b-hr--thick" />
-          <div className="l-mt-3">
-            <NavList
-              columns
-              list={[
-                {
-                  title: 'Refusjonskrav Refusjonskrav Refusjonskrav',
-                  url: '#',
-                  meta: 'Frist 22.november'
-                }
-              ]}
-            />
+        <div className="row">
+          <div className="col-xs-12 l-mt-4">
+            <Heading h="h2">Statistikk</Heading>
+            <hr className="b-hr b-hr--thick" />
+            <div className="l-mt-3">
+              <Heading h="h3" className="h5">
+                Nyeste statistikk
+              </Heading>
+            </div>
+            <div className="row">
+              <div className="col-xs-12 col-md-6 l-mt-2">
+                <Card
+                  heading="Pakkeforløp for brystkreft"
+                  text="Kvalitetsindikator"
+                  url="#"
+                >
+                  <Statistics
+                    bigText="91,3%"
+                    smallText="Andel pakkeforløp for brystkreft som er gjennomført innenfor makisemal anbefalt forløstid."
+                    icon="./icons/pie.svg"
+                  />
+                </Card>
+              </div>
+              <div className="col-xs-12 col-md-6 l-mt-2">
+                <Card
+                  heading="Fastlegestatistikk"
+                  text="Kvalitetsindikator"
+                  url="#"
+                >
+                  <Statistics
+                    bigText="1106"
+                    smallText="Antall på en gjennomsnittlig liste for fastleger."
+                    icon="./icons/pie.svg"
+                  />
+                </Card>
+              </div>
+            </div>
           </div>
         </div>
       </div>
