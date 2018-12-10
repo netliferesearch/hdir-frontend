d080ea197ae18d9c4b2d6ee8b30bd5dea8fd9ff5
olemagnus <olestovern@gmail.com>
2018-12-10 16:36:49 +0100

    Makes button get secondary style

--- src/components/Button.js
+++ src/components/Button.js
@@ -2,16 +2,19 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import classNames from 'classnames';
 
-const buttonClasses = (arrow, small) =>
+const buttonClasses = (arrow, small, secondary) =>
   classNames({
     'b-button__button': true,
+    'b-button__button--secondary': secondary,
     'b-button__button--arrow': arrow,
     'b-button__button--small': small
   });
 
 const Button = props => (
   <div className="b-button">
-    <button className={buttonClasses(props.arrow, props.small)}>
+    <button
+      className={buttonClasses(props.arrow, props.small, props.secondary)}
+    >
       {props.children}
     </button>
   </div>
@@ -20,7 +23,8 @@ const Button = props => (
 Button.propTypes = {
   icon: PropTypes.string,
   arrow: PropTypes.bool,
-  small: PropTypes.bool
+  small: PropTypes.bool,
+  secondary: PropTypes.bool
 };
 
 export default Button;
