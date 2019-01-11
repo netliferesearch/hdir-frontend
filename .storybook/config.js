30d6923fc546343138af80805fc1b5fa1328e02c
olemagnus <olestovern@gmail.com>
2019-01-11 13:34:43 +0100

    Adds loki back: should work after storybook upgrade

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,4 +1,5 @@
 import { configure, setAddon, addDecorator } from '@storybook/react';
+import 'loki/configure-react';
 import staticMarkup from 'react-storybook-addon-static-markup';
 import { withNotes } from '@storybook/addon-notes';
 import '../src/styles/App.scss';
