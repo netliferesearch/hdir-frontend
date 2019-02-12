01a45687efd406d4a92a8770d7ba4554e89bd254
olemagnus <olestovern@gmail.com>
2019-02-12 11:05:00 +0100

    Makes use of the hooks api

--- src/components/ScrollToTop.js
+++ src/components/ScrollToTop.js
@@ -1,19 +1,14 @@
 // This is a React specific component for this project.
 // It makes the React router scroll to the top when navigating to a new page.
 
-import React, { Component } from 'react';
+import React, { useEffect } from 'react';
 import { withRouter } from 'react-router-dom';
 
-class ScrollToTop extends Component {
-  componentDidUpdate(prevProps) {
-    if (this.props.location.pathname !== prevProps.location.pathname) {
-      window.scrollTo(0, 0);
-    }
-  }
-
-  render() {
-    return <>{this.props.children}</>;
-  }
-}
+const ScrollToTop = React.memo(props => {
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  });
+  return <>{props.children}</>;
+});
 
 export default withRouter(ScrollToTop);
