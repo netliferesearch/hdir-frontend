77dfeb1adb0dd668c4c41e80bac966949ee16d69
olemagnus <olestovern@gmail.com>
2019-02-15 14:03:02 +0100

    Adds unique keys

--- src/components/LinkList.js
+++ src/components/LinkList.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import shortid from 'shortid';
 
 const LinkList = props => (
   <nav className="b-link-list">
     {props.list.map(item =>
       item.active ? (
-        <div className="b-link-list__item b-link-list__item--active">
+        <div
+          className="b-link-list__item b-link-list__item--active"
+          key={shortid.generate()}
+        >
           <img
             src={item.image}
             alt={item.imageAlt}
@@ -14,7 +18,11 @@ const LinkList = props => (
           <span className="b-link-list__text">{item.text}</span>
         </div>
       ) : (
-        <a href={item.href} className="b-link-list__item">
+        <a
+          href={item.href}
+          className="b-link-list__item"
+          key={shortid.generate()}
+        >
           <img
             src={item.image}
             alt={item.imageAlt}
