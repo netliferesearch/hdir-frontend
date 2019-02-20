275a71174ed03ab9d909154c987a9312acf59960
olemagnus <olestovern@gmail.com>
2019-02-20 14:56:27 +0100

    Adds new blockquote design and example

--- src/components/Quote.js
+++ src/components/Quote.js
@@ -1,5 +1,10 @@
 import React from 'react';
 
-const Quote = props => <q className="b-quote">{props.children}</q>;
+const Quote = props => (
+  <div className="b-quote">
+    <q className="b-quote__blockquote">{props.children}</q>
+    <div className="b-quote__source">Kilde: Wikipedia</div>
+  </div>
+);
 
 export default Quote;
