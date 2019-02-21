ca90bca507bb490f8b94fa5a1e4541c52df96d1a
olemagnus <olestovern@gmail.com>
2019-02-21 09:48:52 +0100

    Changes source and adds verrtical spacing for paragraphs

--- src/components/Quote.js
+++ src/components/Quote.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Quote = props => (
   <div className="b-quote">
     <q className="b-quote__blockquote">{props.children}</q>
-    <div className="b-quote__source">Kilde: Wikipedia</div>
+    <div className="b-quote__source">Kilde: lovdata.no</div>
   </div>
 );
 
