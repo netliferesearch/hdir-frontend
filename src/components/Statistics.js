5ba6c013c3cf7aecf429479b44258ead1cbbdd2e
olemagnus <olestovern@gmail.com>
2019-02-25 14:18:56 +0100

    makes statistics ignore empty fields, and centers the text

--- src/components/Statistics.js
+++ src/components/Statistics.js
@@ -1,12 +1,25 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import classNames from 'classnames';
+
+const textWrapperClasses = flex =>
+  classNames({
+    'b-statistics__text-wrapper': true,
+    'b-statistics__text-wrapper--flex': flex
+  });
 
 const Statistics = props => (
   <div className="b-statistics">
-    <img className="b-statistics__icon" src={props.icon} alt="" />
-    <div>
-      <div className="b-statistics__big-text">{props.bigText}</div>
-      <div className="b-statistics__small-text">{props.smallText}</div>
+    {props.icon && (
+      <img className="b-statistics__icon" src={props.icon} alt="" />
+    )}
+    <div className={textWrapperClasses(props.flexText)}>
+      {props.bigText && (
+        <div className="b-statistics__big-text">{props.bigText}</div>
+      )}
+      {props.smallText && (
+        <div className="b-statistics__small-text">{props.smallText}</div>
+      )}
     </div>
   </div>
 );
@@ -14,7 +27,8 @@ const Statistics = props => (
 Statistics.propTypes = {
   bigText: PropTypes.string,
   smallText: PropTypes.string,
-  icon: PropTypes.string
+  icon: PropTypes.string,
+  flexText: PropTypes.bool
 };
 
 export default Statistics;
