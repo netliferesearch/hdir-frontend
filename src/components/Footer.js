7467afefe07224c1e0bcf9b14e72b574b266aeff
olemagnus <olestovern@gmail.com>
2019-03-13 16:32:49 +0100

    Removes footer class

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -22,43 +22,41 @@ const Footer = () => (
       </div>
 
       <div className="col-lg-7 col-sm-8 col-xs-12">
-        <div className="b-footer__link-list">
-          <div className="row">
-            <div className="col-xs-12 col-md-6 col-lg-4">
-              <a href="#a" className="b-footer__link">
-                Om oss
-              </a>
-            </div>
-            <div className="col-xs-12 col-md-6 col-lg-4">
-              <a href="#a" className="b-footer__link">
-                Kontakt oss
-              </a>
-            </div>
-            <div className="col-xs-12 col-md-6 col-lg-4">
-              <a href="#a" className="b-footer__link">
-                Nyhetsbrev
-              </a>
-            </div>
-            <div className="col-xs-12 col-md-6 col-lg-4">
-              <a href="#a" className="b-footer__link">
-                Presse
-              </a>
-            </div>
-            <div className="col-xs-12 col-md-6 col-lg-4">
-              <a href="#a" className="b-footer__link">
-                Ledige stillinger
-              </a>
-            </div>
-            <div className="col-xs-12 col-md-6 col-lg-4">
-              <a href="#a" className="b-footer__link">
-                Personvern
-              </a>
-            </div>
-            <div className="col-xs-12 col-md-6 col-lg-4">
-              <a href="#a" className="b-footer__link">
-                Personvern
-              </a>
-            </div>
+        <div className="row">
+          <div className="col-xs-12 col-md-6 col-lg-4">
+            <a href="#a" className="b-footer__link">
+              Om oss
+            </a>
+          </div>
+          <div className="col-xs-12 col-md-6 col-lg-4">
+            <a href="#a" className="b-footer__link">
+              Kontakt oss
+            </a>
+          </div>
+          <div className="col-xs-12 col-md-6 col-lg-4">
+            <a href="#a" className="b-footer__link">
+              Nyhetsbrev
+            </a>
+          </div>
+          <div className="col-xs-12 col-md-6 col-lg-4">
+            <a href="#a" className="b-footer__link">
+              Presse
+            </a>
+          </div>
+          <div className="col-xs-12 col-md-6 col-lg-4">
+            <a href="#a" className="b-footer__link">
+              Ledige stillinger
+            </a>
+          </div>
+          <div className="col-xs-12 col-md-6 col-lg-4">
+            <a href="#a" className="b-footer__link">
+              Personvern
+            </a>
+          </div>
+          <div className="col-xs-12 col-md-6 col-lg-4">
+            <a href="#a" className="b-footer__link">
+              Personvern
+            </a>
           </div>
         </div>
       </div>
