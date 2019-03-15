c4e1dd6fd95475e0629bb5f131e9f50e11c6e3de
olemagnus <olestovern@gmail.com>
2019-03-15 11:01:08 +0100

    wip

--- src/components/MultiSelector.js
+++ src/components/MultiSelector.js
@@ -11,64 +11,97 @@ class MultiSelector extends React.Component {
     this.state = {
       open: false
     };
+    this.openSelector = this.openSelector.bind(this);
+    this.closeSelector = this.closeSelector.bind(this);
+  }
+
+  setSelectorOpen = open => () => {
+    this.setState({ open: open }, () => {
+      // open ? this.domNode.focus() : this.domNode.blur();
+    });
+  };
+
+  openSelector = this.setSelectorOpen(true);
+  closeSelector = this.setSelectorOpen(false);
+
+  componentDidMount() {
+    this.domNode.addEventListener('blur', this.closeSelector);
+  }
+
+  componentWillUnmount() {
+    this.domNode.removeEventListener('blur', this.closeSelector);
   }
 
   render() {
     return (
-      <div className="b-multi-selector">
-        <div className="b-multi-selector__box">
-          <div className="b-multi-selector__checkboxes">
-            <CheckboxGroup
-              name="statistikktype"
-              options={[
-                {
-                  value: 'Kommunalt pasient- og brukerregister',
-                  label: 'Kommunalt pasient- og brukerregister'
-                },
-                {
-                  value: 'Kvalitetsindikator',
-                  label: 'Kvalitetsindikator'
-                },
-                {
-                  value: 'Norsk pasientregister',
-                  label: 'Norsk pasientregister'
-                },
-                {
-                  value: 'Kommunalt pasient- og brukerregister',
-                  label: 'Kommunalt pasient- og brukerregister'
-                },
-                {
-                  value: 'Kvalitetsindikator',
-                  label: 'Kvalitetsindikator'
-                },
-                {
-                  value: 'Norsk pasientregister',
-                  label: 'Norsk pasientregister'
-                },
-                {
-                  value: 'Kommunalt pasient- og brukerregister',
-                  label: 'Kommunalt pasient- og brukerregister'
-                },
-                {
-                  value: 'Kvalitetsindikator',
-                  label: 'Kvalitetsindikator'
-                },
-                {
-                  value: 'Norsk pasientregister',
-                  label: 'Norsk pasientregister'
-                }
-              ]}
-            />
+      <div
+        className="b-multi-selector"
+        ref={ref => {
+          this.domNode = ref;
+        }}
+        tabIndex="0"
+      >
+        <button
+          className="b-multi-selctor__select"
+          onClick={this.state.open ? this.closeSelector : this.openSelector}
+        >
+          {this.state.open ? 'Lukk' : 'Åpne'}
+        </button>
+        {this.state.open && (
+          <div className="b-multi-selector__box">
+            <div className="b-multi-selector__checkboxes">
+              <CheckboxGroup
+                name="statistikktype"
+                options={[
+                  {
+                    value: 'Kommunalt pasient- og brukerregister',
+                    label: 'Kommunalt pasient- og brukerregister'
+                  },
+                  {
+                    value: 'Kvalitetsindikator',
+                    label: 'Kvalitetsindikator'
+                  },
+                  {
+                    value: 'Norsk pasientregister',
+                    label: 'Norsk pasientregister'
+                  },
+                  {
+                    value: 'Kommunalt pasient- og brukerregister',
+                    label: 'Kommunalt pasient- og brukerregister'
+                  },
+                  {
+                    value: 'Kvalitetsindikator',
+                    label: 'Kvalitetsindikator'
+                  },
+                  {
+                    value: 'Norsk pasientregister',
+                    label: 'Norsk pasientregister'
+                  },
+                  {
+                    value: 'Kommunalt pasient- og brukerregister',
+                    label: 'Kommunalt pasient- og brukerregister'
+                  },
+                  {
+                    value: 'Kvalitetsindikator',
+                    label: 'Kvalitetsindikator'
+                  },
+                  {
+                    value: 'Norsk pasientregister',
+                    label: 'Norsk pasientregister'
+                  }
+                ]}
+              />
+            </div>
+            <div className="b-multi-selector__buttons">
+              <Button secondary small onClick={this.closeSelector}>
+                Bekreft temaer
+              </Button>
+              <Button plain small onClick={this.closeSelector}>
+                Avbryt
+              </Button>
+            </div>
           </div>
-          <div className="b-multi-selector__buttons">
-            <Button secondary small>
-              Bekreft temaer
-            </Button>
-            <Button plain small>
-              Avbryt
-            </Button>
-          </div>
-        </div>
+        )}
       </div>
     );
   }
