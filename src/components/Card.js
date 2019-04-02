6907e386293d56741a1ba650a3ed644202da45c0
olemagnus <olestovern@gmail.com>
2019-04-02 17:38:57 +0200

    makes the whole card a link

--- src/components/Card.js
+++ src/components/Card.js
@@ -16,16 +16,16 @@ const linkClasses = arrow =>
   });
 
 const Card = props => (
-  <div className={cardClasses(props.horizontal)}>
+  <a href="#a" className={cardClasses(props.horizontal)}>
     {props.children && <div className="b-card__feature">{props.children}</div>}
-    <a href="#a" className={linkClasses(props.arrow)}>
+    <div className={linkClasses(props.arrow)}>
       {props.heading && <h3 className="b-card__heading">{props.heading}</h3>}
       {props.topic && <div className="b-card__topic">{props.topic}</div>}
       {props.text && <div className="b-card__text">{props.text}</div>}
       {props.content && <div>{props.content}</div>}
       {props.mainContent}
-    </a>
-  </div>
+    </div>
+  </a>
 );
 
 Card.propTypes = {
