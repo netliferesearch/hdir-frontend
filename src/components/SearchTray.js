7e03abcad888abb6581b28c2934096d8cda187c5
olemagnus <olestovern@gmail.com>
2019-04-12 14:37:13 +0200

    removes animation

--- src/components/SearchTray.js
+++ src/components/SearchTray.js
@@ -1,16 +1,8 @@
 import React from 'react';
-import classNames from 'classnames';
-
-const searchTrayClasses = open =>
-  classNames({
-    'b-search-tray': true,
-    'b-search-tray--blue': true,
-    'b-search-tray--open': open
-  });
 
 const SearchTray = props => {
   return (
-    <div className={searchTrayClasses(props.open)}>
+    <div className="b-search-tray b-search-tray--blue">
       <div className="b-search-tray__item">{props.children}</div>
     </div>
   );
