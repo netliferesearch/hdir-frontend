4de13e244accce0a3c0ab14de3137c31fa2c190c
olemagnus <olestovern@gmail.com>
2019-04-26 10:20:00 +0200

    removes unused import

--- src/stories/navList.js
+++ src/stories/navList.js
@@ -1,13 +1,7 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
 
-import {
-  withKnobs,
-  text,
-  boolean,
-  object,
-  select
-} from '@storybook/addon-knobs';
+import { withKnobs, text, boolean, object } from '@storybook/addon-knobs';
 import { checkA11y } from '@storybook/addon-a11y';
 
 import NavList from '../components/NavList';
