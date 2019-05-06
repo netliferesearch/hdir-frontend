cf8848d54f4049e9b19f96c0122bef0f1ef970f3
olemagnus <olestovern@gmail.com>
2019-05-06 16:50:01 +0200

    removes storybook button and format code

--- src/pages/FormbuilderTestPage.js
+++ src/pages/FormbuilderTestPage.js
@@ -1,71 +1,150 @@
 import React from 'react';
-import { default as LinkComp } from '../components/Link';
+import Link from '../components/Link';
 
 // Export it so we can use the same markup on 'ArticlePageWithForm'
 export const formbuilderGeneratedMarkup = () => (
   <section data-portal-component-type="part">
     <h2>Testskjema</h2>
-    <div id="xp-formbuilder-introtext"></div>
+    <div id="xp-formbuilder-introtext" />
     <div id="xp-formbuilder-successmessage">
       <p>Takk for skjemaet</p>
     </div>
-    <form id="testskjema" className="ajax-submit xp-formbuilder-form" action="/test/testskjema/_/component/main/0" method="Send inn skjema" encType="multipart/form-data">
+    <form
+      id="testskjema"
+      className="ajax-submit xp-formbuilder-form"
+      action="/test/testskjema/_/component/main/0"
+      method="Send inn skjema"
+      encType="multipart/form-data"
+    >
       <fieldset>
         <div className="xp-form-input">
           <label htmlFor="Navn">
             <span>Navn</span>
             <span className="required-star">*</span>
-            <input type="text" id="Navn" name="Navn" value="" size="35" placeholder="Her skriver du navnet ditt" className="xp-input" required="required" onChange={() => {}}/>
+            <input
+              type="text"
+              id="Navn"
+              name="Navn"
+              value=""
+              size="35"
+              placeholder="Her skriver du navnet ditt"
+              className="xp-input"
+              required="required"
+              onChange={() => {}}
+            />
           </label>
         </div>
         <div className="xp-form-input">
           <label htmlFor="E-postadresse">
             <span>E-postadresse</span>
             <span className="required-star">*</span>
-            <input type="email" id="E-postadresse" name="E-postadresse" value="" size="35" placeholder="Din e-postadresse" className="xp-input" required="required" onChange={() => {}}/>
+            <input
+              type="email"
+              id="E-postadresse"
+              name="E-postadresse"
+              value=""
+              size="35"
+              placeholder="Din e-postadresse"
+              className="xp-input"
+              required="required"
+              onChange={() => {}}
+            />
           </label>
         </div>
         <div className="xp-form-input">
           <label htmlFor="Fortell%20om%20deg%20selv">
             <span>Fortell om deg selv</span>
           </label>
-          <textarea cols="10" rows="5" type="textarea" id="Fortell%20om%20deg%20selv" name="Fortell%20om%20deg%20selv" size="34" placeholder="Skriv litt om deg selv" className="xp-input"/>
+          <textarea
+            cols="10"
+            rows="5"
+            type="textarea"
+            id="Fortell%20om%20deg%20selv"
+            name="Fortell%20om%20deg%20selv"
+            size="34"
+            placeholder="Skriv litt om deg selv"
+            className="xp-input"
+          />
         </div>
         <div className="xp-form-input">
           <label htmlFor="Hva%20er%20din%20rolle%20i%20prosjektet%3F">
             <span>Hva er din rolle i prosjektet?</span>
           </label>
           <div>
-            <input type="radio" id="Hva%20er%20din%20rolle%20i%20prosjektet%3F" name="Hva%20er%20din%20rolle%20i%20prosjektet%3F" value="utvikler" text="option" checked="checked" onChange={() => {}}/>Utvikler
-            <br/>
+            <input
+              type="radio"
+              id="Hva%20er%20din%20rolle%20i%20prosjektet%3F"
+              name="Hva%20er%20din%20rolle%20i%20prosjektet%3F"
+              value="utvikler"
+              text="option"
+              checked="checked"
+              onChange={() => {}}
+            />
+            Utvikler
+            <br />
           </div>
           <div>
-            <input type="radio" id="Hva%20er%20din%20rolle%20i%20prosjektet%3F" name="Hva%20er%20din%20rolle%20i%20prosjektet%3F" value="prosjektleder" text="option" onChange={() => {}}/>Prosjektleder
-            <br/>
+            <input
+              type="radio"
+              id="Hva%20er%20din%20rolle%20i%20prosjektet%3F"
+              name="Hva%20er%20din%20rolle%20i%20prosjektet%3F"
+              value="prosjektleder"
+              text="option"
+              onChange={() => {}}
+            />
+            Prosjektleder
+            <br />
           </div>
         </div>
         <div className="xp-form-input">
           <label htmlFor="Hva%20er%20din%20alder%3F">
             <span>Hva er din alder?</span>
           </label>
-          <select id="Hva%20er%20din%20alder%3F" name="Hva%20er%20din%20alder%3F">
-            <option value="" selected="selected" disabled="disabled">Velg alder</option>
-            <option value="1829" text="option">18-29</option>
-            <option value="3039" text="option">30-39</option>
-            <option value="4049" text="option">40-49</option>
-            <option value="5059" text="option">50-59</option>
+          <select
+            id="Hva%20er%20din%20alder%3F"
+            name="Hva%20er%20din%20alder%3F"
+          >
+            <option value="" selected="selected" disabled="disabled">
+              Velg alder
+            </option>
+            <option value="1829" text="option">
+              18-29
+            </option>
+            <option value="3039" text="option">
+              30-39
+            </option>
+            <option value="4049" text="option">
+              40-49
+            </option>
+            <option value="5059" text="option">
+              50-59
+            </option>
           </select>
         </div>
         <div className="xp-form-input">
           <label htmlFor="Jeg%20aksepterer%20at%20skjemaet%20sendes%20inn">
-            <input type="checkbox" id="Jeg%20aksepterer%20at%20skjemaet%20sendes%20inn" name="Jeg%20aksepterer%20at%20skjemaet%20sendes%20inn" className="xp-form-checkbox"/>
+            <input
+              type="checkbox"
+              id="Jeg%20aksepterer%20at%20skjemaet%20sendes%20inn"
+              name="Jeg%20aksepterer%20at%20skjemaet%20sendes%20inn"
+              className="xp-form-checkbox"
+            />
             <span>Jeg aksepterer at skjemaet sendes inn</span>
           </label>
         </div>
         <div className="xp-form-input">
           <label htmlFor="Last%20opp%20CV">
             <span>Last opp CV</span>
-            <input type="file" id="Last%20opp%20CV" name="Last%20opp%20CV" value="" size="34" className="xp-input" capture="false" onChange={() => {}}/>
+            <input
+              type="file"
+              id="Last%20opp%20CV"
+              name="Last%20opp%20CV"
+              value=""
+              size="34"
+              className="xp-input"
+              capture="false"
+              onChange={() => {}}
+            />
           </label>
         </div>
         <div className="xp-form-input">
@@ -74,35 +153,56 @@ export const formbuilderGeneratedMarkup = () => (
           </label>
           <div>
             <label>
-              <input type="checkbox" id="Hva%20er%20din%20arbeidserfaring%3F" name="Hva%20er%20din%20arbeidserfaring%3F" value="html" text="option" onChange={() => {}}/>Har erfaring med HTML og CSS
+              <input
+                type="checkbox"
+                id="Hva%20er%20din%20arbeidserfaring%3F"
+                name="Hva%20er%20din%20arbeidserfaring%3F"
+                value="html"
+                text="option"
+                onChange={() => {}}
+              />
+              Har erfaring med HTML og CSS
             </label>
             <label>
-              <input type="checkbox" id="Hva%20er%20din%20arbeidserfaring%3F" name="Hva%20er%20din%20arbeidserfaring%3F" value="js" text="option" onChange={() => {}}/>Har erfaring med JavaScript
+              <input
+                type="checkbox"
+                id="Hva%20er%20din%20arbeidserfaring%3F"
+                name="Hva%20er%20din%20arbeidserfaring%3F"
+                value="js"
+                text="option"
+                onChange={() => {}}
+              />
+              Har erfaring med JavaScript
             </label>
             <label>
-              <input type="checkbox" id="Hva%20er%20din%20arbeidserfaring%3F" name="Hva%20er%20din%20arbeidserfaring%3F" value="cnet" text="option" onChange={() => {}}/>Har erfaring med C# og .NET
+              <input
+                type="checkbox"
+                id="Hva%20er%20din%20arbeidserfaring%3F"
+                name="Hva%20er%20din%20arbeidserfaring%3F"
+                value="cnet"
+                text="option"
+                onChange={() => {}}
+              />
+              Har erfaring med C# og .NET
             </label>
           </div>
         </div>
       </fieldset>
-      <input id="xp-formbuilder-submit" className="btn btn-primary xp-submit" type="submit" value="Send inn skjema" data-captcha="false"/>
+      <input
+        id="xp-formbuilder-submit"
+        className="btn btn-primary xp-submit"
+        type="submit"
+        value="Send inn skjema"
+        data-captcha="false"
+      />
     </form>
   </section>
 );
 
 const FormbuilderTestPage = () => (
-  <>
-    <div className="l-container l-mt-3">
-      <div className="l-mt-2">
-        <LinkComp buttonSecondary href="/storybook">
-          Go to Storybook
-        </LinkComp>
-      </div>
-    </div>
-    <div className="l-container l-mt-3 l-mb-5">
-      {formbuilderGeneratedMarkup()}
-    </div>
-  </>
+  <div className="l-container l-mt-3 l-mb-5">
+    {formbuilderGeneratedMarkup()}
+  </div>
 );
 
 export default FormbuilderTestPage;
