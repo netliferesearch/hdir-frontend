34ea1fbe2e0a0fee9c6085d41a855afeaf18a2d1
olemagnus <olestovern@gmail.com>
2019-05-06 17:26:42 +0200

    moves iconwithtext to its own block

--- src/components/IconWithText.js
+++ src/components/IconWithText.js
@@ -2,19 +2,15 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 const IconWithText = props => (
-  <div className="b-icon--with-text">
+  <div className="b-icon-with-text">
     <img
       src={props.icon}
       alt={props.text}
       role="presentation"
-      className="b-icon--with-text__image"
+      className="b-icon-with-text__image"
       aria-hidden
     />
-    <span
-      className="b-icon--with-text__text"
-    >
-      {props.text}
-    </span>
+    <span className="b-icon-with-text__text">{props.text}</span>
   </div>
 );
 
