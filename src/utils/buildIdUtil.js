4633b877edde13c80841a68874082ea1b9b3963f
Wallace Sidhrée <i@dreamyguy.com>
2019-06-14 13:05:27 +0200

    :shower: Remove unnecessary escape character from regex

--- src/utils/buildIdUtil.js
+++ src/utils/buildIdUtil.js
@@ -1,15 +1,15 @@
 const buildId = text => {
   // Regex for special characters
-  const rgx = /[;\\/:*!%?\"()<>|&'_]/g;
+  const rgx = /[;\\/:*!%?"()<>|&'_]/g;
   // If first character is a number add a string to the start
   if (!isNaN(text.charAt(0))) { text = `header-${text}` }
   return text.toLowerCase()
     // Replace spaces
     .replace(/\s+/g, '-')
     // Replace Norwegian spacial characters
-    .replace(/\æ+/g, 'ae')
-    .replace(/\ø+/g, 'o')
-    .replace(/\å+/g, 'aa')
+    .replace(/æ+/g, 'ae')
+    .replace(/ø+/g, 'o')
+    .replace(/å+/g, 'aa')
     // Replace special characters
     .replace(rgx, '');
 }
