a8bf066fba1fb2873fe6194f238c9d92f4ae040f
thomaslgj <thomas@netliferesearch.com>
2019-07-04 15:37:15 +0200

    Remove createUniqueHeaders from GenerateLinksOnHeadings

--- src/components/GenerateLinksOnHeadings.js
+++ src/components/GenerateLinksOnHeadings.js
@@ -9,15 +9,10 @@ import scrollToTitleFromUrlHash from './../utils/scrollToTitleFromUrlHashUtil';
 
 class GenerateLinksOnHeadings extends Component {
   componentDidMount(){
-    // Find all h2 headers and add unique ids
-    createUniqueHeaders([...document.querySelectorAll('h2')]);
     // Initiate logic for scrolling to header/section based on hash in url
     scrollToTitleFromUrlHash();
   }
   componentDidUpdate(){
-    // Find all h2 headers and add unique ids
-    // Running it on component update as well, to trigger new header IDs on router change
-    createUniqueHeaders([...document.querySelectorAll('h2')]);
     // Initiate logic for scrolling to header/section based on hash in url
     scrollToTitleFromUrlHash();
   }
