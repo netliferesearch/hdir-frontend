ace3a97083f3e994b513e725794a8858811ab153
thomaslgj <thomas@netliferesearch.com>
2019-11-19 11:25:13 +0100

    Change front page text

--- src/stories/welcome.js
+++ src/stories/welcome.js
@@ -5,8 +5,8 @@ import Wrapper from './components/Wrapper.js'
 
 storiesOf('Velkommen', module)
   .add('Kom i gang', () => (
-    <Wrapper title="Kom i gang" lead="Helsedirektoratet og HELFO på nett.">
-      <p>Formålet med denne "Storybooken" er å dokumentere alle delene av nettsidene, i form av komponenter. </p>
+    <Wrapper title="Kom i gang" lead="HDIR og HELFO: Digital designmanual">
+      <p>Formålet med denne digitale designmanualen er å dokumentere alle delene av nettsidene, i form av komponenter. </p>
       <p>En komponent er en bestanddel av en større helhet. En nettside er bygd opp av mange deler, som bokser, titler, bilder, lister osv. Disse er komponentene nettsiden består av. I designsystemet må vi fokusere både på komponentene og helheten.</p>
       <p>Under malverk er komponentene satt sammen til helhetlige sidemaler. Her kommer konteksten bedre frem, og ved endringer kan vi lettere vurdere om det er komponenten i seg selv vi må endre på, eller hvordan den settes sammen med andre, når vi kan se den i kontekst.</p>
       <p>I tillegg er det grunnstiler samt bilder og ikoner, som er mer grunnleggende forutsetninger for hvordan ting skal se ut. Farger, fonter, visuell stil.</p>
