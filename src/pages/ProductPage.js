43b19a9a6325a44961ba6378b7f9c074f3d3687b
thomaslgj <thomas@netliferesearch.com>
2020-03-17 14:16:24 +0100

    Remove extra boxes on ProductPage

--- src/pages/ProductPage.js
+++ src/pages/ProductPage.js
@@ -129,62 +129,6 @@ const ProductPage = () => (
           </div>
         </div>
       </div>
-        <div className="col-xs-12 l-mt-2">
-          <Box>
-            <h3 className="h4">RELATERTE PRODUKTER</h3>
-            <div className="l-mt-1">
-              <NavList
-                small
-                list={[
-                  {
-                    description: 'Svangerskapsomsorgen',
-                    title: 'Nasjonale faglige retningslinjer',
-                    url: '#'
-                  },
-                  {
-                    description: 'BROSJYRE (PDF)',
-                    title:
-                      'Hva kan det være? Svangerskapsdiabetes? Disse symptomene må du være obs på.',
-                    url: '#'
-                  }
-                ]}
-              />
-            </div>
-          </Box>
-        </div>
-        <div className="col-xs-12 l-mt-2">
-          <Box color="green">
-            <ChapterHeading
-              heading="Materiell til helsekontorer"
-              subheading="LAST NED TRYKKSAKER"
-              h="h3"
-              line="none"
-            />
-            <NavList
-              small
-              list={[
-                {
-                  title: 'Det foreslås at HbA1c tas ved',
-                  url: '#',
-                  meta: 'Plakat',
-                  infoText: 'PDF (1,0 MB)',
-                  file: true
-                },
-                {
-                  title:
-                    'Tilby glukosebelastning til gravide i uke 24-28 med en eller flere karakteristika (alder, etnisitet, arvelighet, kroppsmasseindeks og hendelser i tidligere svangerskap)',
-                  url: '#a',
-                  meta: 'Brosjyre',
-                  infoText: 'PDF (3,5 MB)',
-                  file: true
-                }
-              ]}
-            />
-            <div className="l-mt-2">
-              <Button>Last ned alle</Button>
-            </div>
-          </Box>
-        </div>
     </main>
     <div className="l-mt-2">
       <PageMeta
