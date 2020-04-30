01746aadc3f1567916956e1c1c2bc5fcd5721ea0
thomaslgj <thomaslgj@gmail.com>
2020-04-30 10:49:09 +0200

    Use ProductSearch markup that works with native js toggle function

--- src/components/ProductSearch.js
+++ src/components/ProductSearch.js
@@ -4,17 +4,16 @@ import classNames from 'classnames';
 import InputSearch from './InputSearch'
 import Button from './Button'
 
-const mainClasses = (label, toggled) =>
+const mainClasses = () =>
   classNames({
     'b-product-search': true,
-    'b-product-search__label': label,
-    'b-product-search__content': toggled,
   });
-  
-const buttonClasses = () =>
+
+const contentClasses = (toggled) =>
   classNames({
-    'b-button': true,
-    'b-button--plain': true,
+    'b-product-search': true,
+    'b-product-search__content': toggled,
+    'hidden': !toggled
   });
 
 const ProductSearch = ({ label, fnChange }) => {
@@ -27,26 +26,26 @@ const ProductSearch = ({ label, fnChange }) => {
   
   return (
     <div className={mainClasses()}>
-      <Button 
+      <button 
+        id="product-search-trigger"
         onClick={toggle}
-        clean
-        icon={!toggled ? '../icons/search.svg' : '../icons/'}>
-        {
-          toggled ? 'Skjul søk' : label
-        }
-      </Button>
-      {
-        toggled && (
-          <div className={mainClasses(false, toggled)}>
-            <InputSearch
-              label={label}
-              autoFocus={toggled}
-              showSuggestions={false}
-              fnChange={toggled ? fnChange : null}
-            />
-          </div>
-        )
-      }
+        className="b-button b-button--clean">
+        <span
+          className="b-link__icon"
+          role="presentation"
+          style={{ background: 'url(../icons/search.svg)'}}
+        ></span>
+        <span className="b-button__underline">{ toggled ? 'Skjul søk' : label }</span>
+      </button>
+      <div id="product-search" className={contentClasses(toggled)}>
+        <InputSearch
+          id="product-search-input"
+          label={label}
+          autoFocus={toggled}
+          showSuggestions={false}
+          fnChange={toggled ? fnChange : null}
+        />
+      </div>
     </div>
   );
 }
