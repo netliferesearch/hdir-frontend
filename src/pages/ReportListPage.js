e33ab3cbb0617aa0d661078ebb153934b50ef285
thomaslgj <thomaslgj@gmail.com>
2020-05-12 10:27:19 +0200

    New layout on ReportListPage

--- src/pages/ReportListPage.js
+++ src/pages/ReportListPage.js
@@ -1,67 +1,73 @@
 import React from 'react';
-
 import List from '../components/List';
 import Button from '../components/Button';
 import ListHeading from '../components/ListHeading';
 import CheckboxGroup from '../components/CheckboxGroup';
 import Box from '../components/Box';
-import Card from '../components/Card';
+import Breadcrumbs from '../components/Breadcrumbs';
 import ArticleIntro from '../components/ArticleIntro';
+import ProductSearch from '../components/ProductSearch';
+import { dummyFlatTree } from '../components/dummyFlatTree';
 
 const ReportListPage = () => (
-  <main id="main">
-    <div className="l-container">
-      <div className="row l-mt-3">
-        <div className="col-xs-12">
-          <ArticleIntro
-            heading="Rapporter"
-            lead="Rapporter, rundskriv, strategier, brosjyrer, kort og plakater. Denne
-          teksten skal bli mer utfyllende, og bedre."
-          />
-        </div>
-      </div>
-      <div className="row l-mt-2">
-        <div className="col-xs-12">
-          <span>NYESTE RAPPORTER</span>
-        </div>
-        <div className="col-xs-12 col-sm-6 l-mt-1">
-          <Card
-            heading="Ventetider og pasientrettigheter 2017"
-            text="17. mars 2018 "
-            url="#a"
-            arrow="right"
-          >
-            <Box square color="blueDark">
-              <div className="h1 t-sm-center">-3</div>
-              <div className="t-sm-center">
-                Ventetiden gikk ned med tre dager fra 2016 til 2017.
-              </div>
-            </Box>
-          </Card>
-        </div>
-        <div className="col-xs-12 col-sm-6 l-mt-1">
-          <Card
-            heading="Ventetider og pasientrettigheter 2017"
-            text="21. januar 2018"
-            url="#a"
-            arrow="right"
-          >
-            <Box square color="blueDark">
-              <div className="h3">
-                Gjennomsnittlig ventetid i 2017 er på 58 dager for somatisk
-                sektor.
-              </div>
-            </Box>
-          </Card>
-        </div>
-      </div>
-      <div className="row l-mt-3">
-        <aside className="col-md-3 col-xs-12 l-mb-4">
-          <div className="h4">Filter</div>
-          <div className="l-mt-1">
-            <hr className="b-hr b-hr--thick" />
+  <div className="l-container">
+    <Breadcrumbs
+      paths={[
+        {
+          name: 'Forsiden',
+          href: '#a'
+        }
+      ]}
+    />
+    <main id="main">
+        <div className="row l-mt-2">
+          <div className="col-xs-12">
+            <ArticleIntro
+              heading="Rapporter"
+              lead="Rapporter, rundskriv, strategier, brosjyrer, kort og plakater. Denne
+            teksten skal bli mer utfyllende, og bedre."
+            />
           </div>
-          <div className="l-mt-1">
+        </div>
+      <div className="l-layout l-mt-2">
+        {/* <div className="row l-mt-2">
+          <div className="col-xs-12">
+            <span>NYESTE RAPPORTER</span>
+          </div>
+          <div className="col-xs-12 col-sm-6 l-mt-1">
+            <Card
+              heading="Ventetider og pasientrettigheter 2017"
+              text="17. mars 2018 "
+              url="#a"
+              arrow="right"
+            >
+              <Box square color="blueDark">
+                <div className="h1 t-sm-center">-3</div>
+                <div className="t-sm-center">
+                  Ventetiden gikk ned med tre dager fra 2016 til 2017.
+                </div>
+              </Box>
+            </Card>
+          </div>
+          <div className="col-xs-12 col-sm-6 l-mt-1">
+            <Card
+              heading="Ventetider og pasientrettigheter 2017"
+              text="21. januar 2018"
+              url="#a"
+              arrow="right"
+            >
+              <Box square color="blueDark">
+                <div className="h3">
+                  Gjennomsnittlig ventetid i 2017 er på 58 dager for somatisk
+                  sektor.
+                </div>
+              </Box>
+            </Card>
+          </div>
+        </div> */}
+        <aside className="l-hide-to-lg l-sidebar l-bleed-right">
+          <hr className="b-hr b-hr--thick" />
+          <div className="l-mt-2">
             <CheckboxGroup
               heading="Velg tema"
               name="statistikktype"
@@ -106,8 +112,16 @@ const ReportListPage = () => (
             />
           </div>
         </aside>
-        <section className="col-xs-12 col-md-8 col-md-offset-1">
-          <ListHeading heading="250 Rapporter" />
+        <article className="l-article l-article--topline">
+          <div className="l-mt-1 l-mb-2">
+            <ProductSearch
+              label="rapporter"
+              productId="0e87de78-2cb4-4a70-93a7-0d687443b71e"
+              flatTree={dummyFlatTree}
+              malGruppe={''}
+              collapsed
+            />
+          </div>
           <div className="js-expand-list" data-start-limit="10">
             <List
               list={[
@@ -261,10 +275,11 @@ const ReportListPage = () => (
               <Button secondary>↓ Vis alle</Button>
             </div>
           </div>
-        </section>
+        </article>
       </div>
-    </div>
-  </main>
+    </main>
+  </div>
+
 );
 
 export default ReportListPage;
