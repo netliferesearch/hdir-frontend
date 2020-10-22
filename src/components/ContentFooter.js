e950ff0f3b2f5752efe5e7dd3ce86d4aa9870920
mari.selnes.netlife <mari.selnes@netlife.com>
2020-10-22 11:31:56 +0200

    Add new component: ContentFooter and style box-small

---
+++ src/components/ContentFooter.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import Button from '../components/Button';
+
+const ContentFooter = props => (
+  <div className="">
+    <Button clean small icon="../icons/chevron.svg">
+      {props.heading}
+    </Button>
+    <div className="b-box-small l-mb-1">
+      <p>{props.content}</p>
+    </div>
+  </div>
+);
+
+ContentFooter.propTypes = {
+  heading: PropTypes.string,
+  content: PropTypes.string
+};
+export default ContentFooter;
